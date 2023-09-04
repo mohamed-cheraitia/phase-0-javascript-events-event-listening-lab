5b1c0c1dca214dab603899d737d7b4133e6790f1
moh1med <mohamedch047@gmail.com>
2023-09-04 14:10:57 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+const input = document.getElementById('button');
+
+function showAlert(){
+    alert('Alert was clicked ! ');
 }
+input.addEventListener('click',showAlert);
+}
+
+addingEventListener();
